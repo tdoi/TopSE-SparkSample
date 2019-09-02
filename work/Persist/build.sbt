c183d8ad4d96c0f4243b646005ef3703bd8708e2
Ubuntu <ubuntu@ip-172-31-22-6.ec2.internal>
2019-09-02 04:59:31 +0000

    Add Persist Sample

---
+++ work/Persist/build.sbt
@@ -0,0 +1,7 @@
+name := "Persist"
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.4.3"
+)
+
---
+++ work/Persist/src/main/scala/jp/topse/Persist.scala
@@ -0,0 +1,32 @@
+package jp.topse
+
+import java.io.PrintWriter
+import org.apache.spark.SparkContext
+import org.apache.spark.storage.StorageLevel._
+
+
+object Persist {
+
+  def main(args: Array[String]) {
+    val sc = new SparkContext("spark://master:7077", "Persist")
+    try {
+      val heavyRDD = sc.parallelize( 1 to 1, 1).map{
+        d => Thread.sleep(5000)
+        d
+      }
+//      heavyRDD.persist(MEMORY_ONLY)
+
+      var start = System.currentTimeMillis
+      heavyRDD.count
+      var end = System.currentTimeMillis
+      println(s"count job: ${end - start}ms")
+
+      start = System.currentTimeMillis
+      heavyRDD.sum
+      end = System.currentTimeMillis
+      println(s"sum job: ${end - start}ms")
+    } finally {
+      sc.stop()
+    }
+  }
+}
